79f2460fc180c8602df9c015b975af7b32f872fc
aclar119 <91166316+aclar119@users.noreply.github.com>
2023-02-06 22:38:30 -0500

    Added footer

---
+++ Components/footer.js
@@ -0,0 +1,42 @@
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+        <style>
+
+            #footer {
+            display: grid;
+            }
+            
+            #footer-line {
+            margin: 0;
+            }
+            
+            #footer-left {
+            padding: 20px;
+            grid-column: 1;
+            text-align: start;
+            }
+            
+            #footer-right {
+            padding: 20px;
+            grid-column: 2;
+            text-align: end;
+            }
+
+        </style>
+
+        <footer>
+            <hr id="footer-line">
+            <div id="footer">
+                <div id="footer-left">@ 2023 ADM 4379 Group 8</div>
+                <div id="footer-right">Privacy Policy and Terms of Service not available</div>
+            </div>
+        </footer>
+    `;
+  }
+}
+customElements.define('footer-component', Footer);
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Group project for ADM 4379
+
+Some elements do not display properly on Firefox.
+This website is best supported by Chrome. 
