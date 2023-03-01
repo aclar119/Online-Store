71b2d9ba914e5f4aad33c0eef0f4cea61e41dc3e
aclar119 <91166316+aclar119@users.noreply.github.com>
2023-03-01 00:23:57 -0500

    DB now populated with products

--- src/Backend/create_database.php
+++ src/Backend/create_database.php
@@ -35,6 +35,9 @@
             );
 
             createTables($mysqli);
+            populateColours($mysqli);
+            populateCategories($mysqli);
+            populateProducts($mysqli);
             
         }
         
@@ -45,13 +48,12 @@
 
         // Defining the structure of all our tables
         $create_table_colours = "CREATE TABLE Colours (
-            ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            Name VARCHAR(40) NOT NULL,
-            HexCode VARCHAR(7) NOT NULL
+            ID INT UNSIGNED PRIMARY KEY,
+            Name VARCHAR(40) NOT NULL
         )";
 
         $create_table_categories = "CREATE TABLE Categories (
-            ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            ID INT UNSIGNED PRIMARY KEY,
             Name VARCHAR(40) NOT NULL
         )";
         
@@ -62,7 +64,7 @@
             Description TEXT NOT NULL,
             ColourID INT NOT NULL,
             FOREIGN KEY (ColourID) REFERENCES Colours(ID),
-            ImagePath VARCHAR(255) NOT NULL,
+            ImageFile VARCHAR(255) NOT NULL,
             CategoryID INT NOT NULL,
             FOREIGN KEY (CategoryID) REFERENCES Categories(ID)
         )";
@@ -111,6 +113,193 @@
 
         $mysqli->query($insert_user_root);
 
+    }
+
+    // Add all colours to the database
+    function populateColours($mysqli) {
+        $colours = array(
+            array("id" => 1, "name" => "Black"),
+            array("id" => 2, "name" => "Blue"),
+            array("id" => 3, "name" => "Brown"),
+            array("id" => 4, "name" => "Green"),
+            array("id" => 5, "name" => "Grey"),
+            array("id" => 6, "name" => "White"),
+            array("id" => 7, "name" => "Yellow")
+        );
+
+        for ($i = 0; $i < count($colours); $i++) {
+            $id = $colours[$i]["id"];
+            $name = $colours[$i]["name"];
+
+            $insert_colour = "INSERT INTO 
+                Colours (ID, Name)
+                VALUES ($id, '$name')
+            ";
+
+            $mysqli->query($insert_colour);
+        }
+    }
+
+    // Add all categories to the database
+    function populateCategories($mysqli) {
+        $categories = array(
+            array("id" => 1, "name" => "T-Shirts"),
+            array("id" => 2, "name" => "Jackets"),
+            array("id" => 3, "name" => "Sweaters")
+        );
+
+        for ($i = 0; $i < count($categories); $i++) {
+            $id = $categories[$i]["id"];
+            $name = $categories[$i]["name"];
+
+            $insert_category = "INSERT INTO 
+                Categories (ID, Name)
+                VALUES ($id, '$name')
+            ";
+            
+            $mysqli->query($insert_category);
+        }
+    }
+
+    function populateProducts($mysqli) {
+        $products = array(
+            array(
+                "name" => "Generic Blue T",
+                "price" => 20.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 2, // Blue
+                "imageFile" => "Generic Blue T.png",
+                "categoryId" => 1, // T-Shirts
+            ),
+            array(
+                "name" => "Generic Black T",
+                "price" => 20.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 1, // Black
+                "imageFile" => "Generic Black T.png",
+                "categoryId" => 1, // T-Shirts
+            ),
+            array(
+                "name" => "Generic Brown T",
+                "price" => 20.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 3, // Brown
+                "imageFile" => "Generic Brown T.png",
+                "categoryId" => 1, // T-Shirts
+            ),
+            array(
+                "name" => "Generic Yellow T",
+                "price" => 20.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 7, // Yellow
+                "imageFile" => "Generic Yellow T.png",
+                "categoryId" => 1, // T-Shirts
+            ),
+            array(
+                "name" => "Generic White T",
+                "price" => 20.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 6, // White
+                "imageFile" => "Generic White T.png",
+                "categoryId" => 1, // T-Shirts
+            ),
+            array(
+                "name" => "Green Jacket",
+                "price" => 40.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 4, // Green
+                "imageFile" => "Green Jacket.png",
+                "categoryId" => 2, // Jackets
+            ),
+            array(
+                "name" => "Blue Denim Jacket",
+                "price" => 40.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 2, // Blue
+                "imageFile" => "Blue Denim Jacket.png",
+                "categoryId" => 2, // Jackets
+            ),
+            array(
+                "name" => "White Jacket",
+                "price" => 40.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 6, // White
+                "imageFile" => "White Jacket.png",
+                "categoryId" => 2, // Jackets
+            ),
+            array(
+                "name" => "Grey Jacket",
+                "price" => 40.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 5, // Grey
+                "imageFile" => "Grey Jacket.png",
+                "categoryId" => 2, // Jackets
+            ),
+            array(
+                "name" => "Black Jacket",
+                "price" => 40.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 1, // Black
+                "imageFile" => "Black Jacket.png",
+                "categoryId" => 2, // Jackets
+            ),
+            array(
+                "name" => "Black Sweater",
+                "price" => 25.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 1, // Black
+                "imageFile" => "Black Sweater.png",
+                "categoryId" => 3, // Sweaters
+            ),
+            array(
+                "name" => "White Sweater",
+                "price" => 25.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 3, // White
+                "imageFile" => "White Sweater.png",
+                "categoryId" => 3, // Sweaters
+            ),
+            array(
+                "name" => "Blue Sweater",
+                "price" => 25.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 2, // Blue
+                "imageFile" => "Blue Sweater.png",
+                "categoryId" => 3, // Sweaters
+            ),
+            array(
+                "name" => "Grey Sweater",
+                "price" => 25.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 5, // Grey
+                "imageFile" => "Grey Sweater.png",
+                "categoryId" => 3, // Sweaters
+            ),
+            array(
+                "name" => "Yellow Sweater",
+                "price" => 25.00,
+                "description" => "This is a placeholder description",
+                "colourId" => 7, // Yellow
+                "imageFile" => "Yellow Sweater.png",
+                "categoryId" => 3, // Sweaters
+            )
+        );
+            
+        for ($i = 0; $i < count($products); $i++) {
+            $name = $products[$i]["name"];
+            $price = $products[$i]["price"];
+            $description = $products[$i]["description"];
+            $colour_id = $products[$i]["colourId"];
+            $image_file = $products[$i]["imageFile"];
+            $category_id = $products[$i]["categoryId"];
+
+            $insert_product = "INSERT INTO 
+                Products (Name, Price, Description, ColourID, ImageFile, CategoryID)
+                VALUES ('$name', $price, '$description', $colour_id, '$image_file', $category_id)
+            ";
+            
+            $mysqli->query($insert_product);
+        }
     }
 
 ?>
